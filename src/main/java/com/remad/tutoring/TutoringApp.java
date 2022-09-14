778a5961ee58c47483e8119cd6137f1c1fa399ad
RenatiusvanSander / R. Meier <remad@web.de>
2022-09-14 21:23:28 +0200

    master: initial commit

---
+++ src/main/java/com/remad/tutoring/TutoringApp.java
@@ -0,0 +1,20 @@
+package com.remad.tutoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Tutoring Spring Boot App
+ */
+@SpringBootApplication
+public class TutoringApp {
+
+  /**
+   * Main to run Spring Boot Application
+   *
+   * @param args arguments from operating system environment
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(TutoringApp.class, args);
+  }
+}
