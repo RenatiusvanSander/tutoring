d44c0d1213eb7af91f1fe68e7e99de3f7d4fa6c2
RenatiusvanSander / R. Meier <remad@web.de>
2022-11-16 00:25:14 +0100

    restcontrollers: ZipCodeControllerTest testCreateZipCode

--- src/test/java/com/remad/tutoring/controller/ZipCodeControllerTest.java
+++ src/test/java/com/remad/tutoring/controller/ZipCodeControllerTest.java
@@ -1,15 +1,21 @@
 package com.remad.tutoring.controller;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.remad.tutoring.controllers.ZipCodeController;
+import com.remad.tutoring.models.ZipCode;
 import com.remad.tutoring.services.ZipCodeService;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -36,6 +42,28 @@ public class ZipCodeControllerTest {
     when(zipCodeService.exists(pathVariableId)).thenReturn(true);
 
     mockMvc.perform(get("/zipcodes/exists/{id}", pathVariableId))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void testDeleteZipCodeShouldReturnHttpStatusOk() throws Exception {
+    long pathVariableId = 5L;
+
+    mockMvc.perform(delete("/zipcodes/{id}", pathVariableId)).andExpect(status().isOk());
+  }
+
+  @Test
+  public void testCreateZipCodeShouldReturnHttpStatusOk() throws Exception {
+    ZipCode zipCodeToCreate = new ZipCode("Hamburg", LocalDateTime.now());
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.findAndRegisterModules();
+    String json = objectMapper.writeValueAsString(zipCodeToCreate);
+
+    mockMvc.perform(
+            post("/zipcodes/create-zipcode", zipCodeToCreate).contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+                .characterEncoding("utf-8"))
         .andExpect(status().isOk());
   }
 }
